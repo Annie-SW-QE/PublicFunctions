92b07aa2a9222579a2855ec98be145b977a4b92b
Annie-SW-QE <163607769+Annie-SW-QE@users.noreply.github.com>
2024-05-10 21:55:42 -0400

    added info on variables passed to the function and created a comparison to throw an error due to length mismatch of label and split_cols

--- CSVMod.py
+++ CSVMod.py
@@ -1,10 +1,24 @@
 #This will be a function that takes a csv file, splits a column, renames those columns and rejoins the dataframes together
+
+#importing pandas and csv modules
 import pandas as pd
 import csv
+#defining the function
+## What is passed to the function? 
+##### file: the csv file to be read; ex. 'test_2.csv'
+##### sep:the delimeter of the majority of the file as a string; ex. ',' , ' ', '/'
+##### col_to_split: the name of the column in the file you want to split along a delimeter as a string; ex. 'Credentials'
+##### split_delim: the delimeter of the column to be split upon; same input type as sep 
+##### labels: the dataframe of new column names of the newly created columns from the split; ex. ['Category 1','Category 2',...]
+######## NOTE: you must have a labels dataframe that matches the length of the newly split columns dataframe or the function, 
+######## or compiler itself will throw an error
 
 def csvmod(file,sep,col_to_split,split_delim,labels):
     file_to_change= pd.read_csv(file,sep=sep,header=0)
     split_cols = file_to_change[col_to_split].str.split(split_delim,expand=True)
+    if len(split_cols) != len(labels):
+        print('Error: mismatched size of label to column frame')
+    else: pass
     file_drop = file_to_change.drop([col_to_split],axis=1)
     new_file = split_cols.set_axis(labels, axis='columns')
     altered_df = file_drop.join(new_file)
